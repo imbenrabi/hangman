97813ae59da04034d8e7d84bc6d72bc8e2b9f664
imbenrabi <imbenrabi@gmail.com>
2020-10-03 19:17:22 +0300

    end end game

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 class EndGame extends Component {
     renderMessage = () => {
-        return this.props.win ? <p>Congarts!</p> : <p>The word is: {this.props.secretWord}</p>
+        if (this.props.depleted === true) {
+            return <p>You have completed the game, come back in 674 days.</p>
+        } else {
+            return this.props.win ? <p>Congarts! The word is: {this.props.secretWord}</p> : <p>Loser! The word is: {this.props.secretWord}</p>
+        }
+    }
+
+    handleRestartClick = () => {
+        this.props.restart();
     }
 
     render() {
         return (
             <div className='end-game'>
                 {this.renderMessage()}
+                <button onClick={this.handleRestartClick} >Restart</button>
             </div>
         );
     }
---
+++ src/solutions/solutions.js
@@ -0,0 +1,22 @@
+export const solutions = [
+    {
+        word: 'SECRET',
+        hint: 'Its a secret.',
+    },
+    {
+        word: 'SMURF',
+        hint: 'Small, blue creature.',
+    },
+    {
+        word: 'TENT',
+        hint: 'Better than a house.',
+    },
+    {
+        word: 'GOAT',
+        hint: 'Greatest of all time.',
+    },
+    {
+        word: 'BB',
+        hint: 'Our king.',
+    }
+]
